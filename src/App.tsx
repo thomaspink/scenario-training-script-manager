74498bc09d730612397cb095398657353f0d9ca1
Thomas Pink <thomas.pink@me.com>
2021-11-02 19:20:30 +0100

    added styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button } from './Button';
 
 function App() {
   return (
     <div className="App">
-      asf
+      <Button></Button>
     </div>
   );
 }
---
+++ src/Button.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+`;
+
+export const Button = () => (
+<StyledButton>
+    asdf
+</StyledButton>
+);
